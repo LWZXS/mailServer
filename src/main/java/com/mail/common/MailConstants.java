3cdda07658e927ae45420ee26a68a5d3e8372660
Jim <2355652781@qq.com>
2018-04-11 16:30:34 -0700

    Jim

--- src/main/java/com/mail/common/MailConstants.java
+++ src/main/java/com/mail/common/MailConstants.java
@@ -1,9 +1,9 @@
 package com.mail.common;
 
 public class MailConstants {
-	public static final String SCEDULEDEMAILINTERVAL = "0 0 6 1/1 * ? *"; //every day at 6:00
+//	public static final String SCEDULEDEMAILINTERVAL = "0 0 6 1/1 * ? *"; //every day at 6:00
 //	public static final String SCEDULEDEMAILINTERVAL = "0 0 6 ? * TUE,FRI *";//every tue, fri at 6:00
-//	public static final String SCEDULEDEMAILINTERVAL = "0 0/1 * 1/1 * ? *";//every one minute
+	public static final String SCEDULEDEMAILINTERVAL = "0 0/1 * 1/1 * ? *";//every one minute
 //	public static final String SCEDULEDEMAILINTERVAL = "0/5 * * * * ?";//every one second
 	
 	public static final Integer SUBSCRIBED = 1;
@@ -12,4 +12,6 @@ public class MailConstants {
 	public static final Long BEFORHOLIDAY = 7L;
 	public static final String TARGETDAY1 = "TUESDAY";
 	public static final String TARGETDAY2 = "FRIDAY";
+
+	public static final String REGULARTEMPLATE = "TestMail";
 }
